cb3e8f3e753137c6060ba07765e9ebf9021c5b35
pozu84 <pozu84@gmail.com>
2022-01-04 22:46:29 +0100

    clase

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-dirPath = r"C:\Users\pozu8\Downloads"
-result = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
-print(result)
+a = 5
+b = 6
+c = 7
 
+resultado = (a < b)
+print(resultado)
+
+# podemos imprimir directamente el resultado de la condicion sin necesidad de asignarlo a una variable
+print(a > b)
+
+resultado2 = (a > 5 and c < 7)
+print(resultado2)
